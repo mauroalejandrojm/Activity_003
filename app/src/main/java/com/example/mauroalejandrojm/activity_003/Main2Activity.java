6027d659185183e5547c319525b93d38c14ffb4e
mauroalejandrojm <jmauro@uninorte.edu.co>
2018-02-19 13:04:22 -0500

    Fixed add and subtract. LayoutGenerator Added.

--- app/src/main/java/com/example/mauroalejandrojm/activity_003/Main2Activity.java
+++ app/src/main/java/com/example/mauroalejandrojm/activity_003/Main2Activity.java
@@ -1,13 +1,105 @@
 package com.example.mauroalejandrojm.activity_003;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
+    private String TAG = "test_00";
+    LinearLayout main2;
+    int count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        main2 = (LinearLayout) findViewById(R.id.other_Layout);
+        count = 1;
+        FloatingActionButton button_plus_00 = findViewById(R.id.Button_plus);
+        button_plus_00.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                generateLayout3();
+                //test();
+            }
+        });
+    }
+    private void generateLayout() {
+        LinearLayout newLayout = new LinearLayout(this);
+        newLayout.setOrientation(LinearLayout.VERTICAL);
+        TextView tv = new TextView(this);
+        EditText et = new EditText(this);
+        Button bt = new Button(this);
+        bt.setText("Button "+count);
+        bt.setTag(count);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("LayoutGeneratorLog","El boton "+view.getTag());
+            }
+        });
+        tv.setText("el tv");
+
+        newLayout.addView(tv);
+        newLayout.addView(et);
+        newLayout.addView(bt);
+        main2.addView(newLayout);
+        count++;
+    }
+    private void generateLayout2() {
+        LinearLayout newLayout = new LinearLayout(this);
+        newLayout.setOrientation(LinearLayout.VERTICAL);
+        TextView tv = new TextView(this);
+        EditText et = new EditText(this);
+        //Edi.setId(count);
+        Button bt = new Button(this);
+        bt.setText("Button "+count);
+        bt.setTag(count);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int id = (int) view.getTag();
+                EditText e = (EditText) findViewById(id);
+                Log.d("LayoutGeneratorLog","El boton "+e.getText());
+            }
+        });
+        tv.setText("el tv");
+
+        newLayout.addView(tv);
+        newLayout.addView(et);
+        newLayout.addView(bt);
+        main2.addView(newLayout);
+
+        count++;
+    }
+    private void generateLayout3() {
+        LinearLayout newLayout = new LinearLayout(this);
+        newLayout.setOrientation(LinearLayout.VERTICAL);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view_00 = inflater.inflate(R.layout.inside_layout, newLayout, false);
+        Button button_00 = (Button) view_00.findViewById(R.id.button_a);
+        final EditText editText_00 = (EditText) view_00.findViewById(R.id.editText_a);
+        button_00.setText("Edit "+count);
+        button_00.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("LayoutGeneratorLog","El boton "+editText_00.getText());
+            }
+        });
+        newLayout.addView(view_00);
+        main2.addView(newLayout);
+        count++;
+    }
+    public void test(){
+        Toast.makeText(this,"nothing",Toast.LENGTH_SHORT);
     }
 }
--- app/src/main/java/com/example/mauroalejandrojm/activity_003/MainActivity.java
+++ app/src/main/java/com/example/mauroalejandrojm/activity_003/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.mauroalejandrojm.activity_003;
 
+import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.Layout;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,24 +16,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private String Tag = "mensaje";
-    private double result,result_c, result_c2;
-    public double op1,op2;
+    LinearLayout main2;
+    private double result, result_c, result_c2;
+    public double op1, op2;
     public int op;
     public EditText display;
     public EditText display_00;
     public Button Button_00;
     public EditText EditText_00;
     public int count;
-    public double result_s[] = new double[10];
+    ArrayList<Double> result_s = new ArrayList<Double>();
     public int i = 2;
     public int j = 0;
     Button add, subs, mult, div, del;
     LinearLayout main;
     ConstraintLayout main_0;
-
 
 
     @Override
@@ -49,25 +53,32 @@ public class MainActivity extends AppCompatActivity {
         del = findViewById(R.id.b_del);
 
     }
-    public void suma (View view){
-        try{
+
+    public void suma(View view) {
+        try {
             String aux = display.getText().toString();
-            op1=Double.parseDouble(aux);
-        } catch (NumberFormatException nfe){};
+            op1 = Double.parseDouble(aux);
+        } catch (NumberFormatException nfe) {
+        }
+        ;
         display.setText("");
-        op=1;
+        op = 1;
         saved();
     }
-    public void resta (View view){
-        try{
+
+    public void resta(View view) {
+        try {
             String aux = display.getText().toString();
-            op1=Double.parseDouble(aux);
-        } catch (NumberFormatException nfe){};
+            op1 = Double.parseDouble(aux);
+        } catch (NumberFormatException nfe) {
+        }
+        ;
         display.setText("");
-        op=2;
+        op = 2;
         saved();
     }
-    public void multi (View view){
+
+    public void multi(View view) {
         try {
             String aux = display.getText().toString();
             op1 = Double.parseDouble(aux);
@@ -77,130 +88,144 @@ public class MainActivity extends AppCompatActivity {
         op = 3;
     }
 
-    public void divi (View view){
-        try{
-            String aux = display.getText().toString();
-            op1=Double.parseDouble(aux);
-        } catch (NumberFormatException nfe){};
-        display.setText("");
-        op=4;
-    }
-    public void igual (View view){
+    public void divi(View view) {
         try {
-            String aux2=display.getText().toString();
-            op2=Double.parseDouble(aux2);
-        } catch(NumberFormatException nfe){}
+            String aux = display.getText().toString();
+            op1 = Double.parseDouble(aux);
+        } catch (NumberFormatException nfe) {
+        }
+        ;
         display.setText("");
-        if (op ==1) {
+        op = 4;
+    }
+
+    public void igual(View view) {
+        try {
+            String aux2 = display.getText().toString();
+            op2 = Double.parseDouble(aux2);
+        } catch (NumberFormatException nfe) {
+        }
+        display.setText("");
+        if (op == 1) {
             result = result_c + op2;
-        }
-        else if(op==2){
+        } else if (op == 2) {
             result = result_c - op2;
-        }
-        else if(op==3){
+        } else if (op == 3) {
             result = op1 * op2;
-        }
-        else if (op==4){
-            if (op2==0){
+        } else if (op == 4) {
+            if (op2 == 0) {
                 display.setText("Error");
-            }
-            else{
-            result = op1 / op2;
+            } else {
+                result = op1 / op2;
             }
         }
-        result_c=result;
-        display.setText(""+result);
+        result_c = result;
+        display.setText("" + result);
         saved2();
     }
-    public void clc (View view) {
+
+    public void clc(View view) {
         display.setText("");
-        op1=0.0;
-        op2=0.0;
-        result=0.0;
-        result_c= 0.0;
+        op1 = 0.0;
+        op2 = 0.0;
+        result = 0.0;
+        result_c = 0.0;
     }
-    public void clear (View view){
-        if (!display.getText().toString().equals("")){
-            display.setText(display.getText().subSequence(0, display.getText().length() - 1)+"");
+
+    public void clear(View view) {
+        if (!display.getText().toString().equals("")) {
+            display.setText(display.getText().subSequence(0, display.getText().length() - 1) + "");
         }
     }
-    public void bt9 (View view) {
+
+    public void bt9(View view) {
         //Toast.makeText(this,"Click en el boton",Toast.LENGTH_SHORT).show();
         //Log.d(Tag, "Click en el boton");
-        String cap=display.getText().toString();
-        cap=cap+"9";
+        String cap = display.getText().toString();
+        cap = cap + "9";
         display.setText(cap);
     }
-    public void bt8 (View view) {
+
+    public void bt8(View view) {
         //Toast.makeText(this,"Click en el boton",Toast.LENGTH_SHORT).show();
         //Log.d(Tag, "Click en el boton");
-        String cap=display.getText().toString();
-        cap=cap+"8";
+        String cap = display.getText().toString();
+        cap = cap + "8";
         display.setText(cap);
     }
-    public void bt7 (View view) {
+
+    public void bt7(View view) {
         //Toast.makeText(this,"Click en el boton",Toast.LENGTH_SHORT).show();
         //Log.d(Tag, "Click en el boton");
-        String cap=display.getText().toString();
-        cap=cap+"7";
+        String cap = display.getText().toString();
+        cap = cap + "7";
         display.setText(cap);
     }
-    public void bt6 (View view) {
+
+    public void bt6(View view) {
         //Toast.makeText(this,"Click en el boton",Toast.LENGTH_SHORT).show();
         //Log.d(Tag, "Click en el boton");
-        String cap=display.getText().toString();
-        cap=cap+"6";
+        String cap = display.getText().toString();
+        cap = cap + "6";
         display.setText(cap);
     }
-    public void bt5 (View view) {
+
+    public void bt5(View view) {
         //Toast.makeText(this,"Click en el boton",Toast.LENGTH_SHORT).show();
         //Log.d(Tag, "Click en el boton");
-        String cap=display.getText().toString();
-        cap=cap+"5";
+        String cap = display.getText().toString();
+        cap = cap + "5";
         display.setText(cap);
     }
-    public void bt4 (View view) {
+
+    public void bt4(View view) {
         //Toast.makeText(this,"Click en el boton",Toast.LENGTH_SHORT).show();
         //Log.d(Tag, "Click en el boton");
-        String cap=display.getText().toString();
-        cap=cap+"4";
+        String cap = display.getText().toString();
+        cap = cap + "4";
         display.setText(cap);
     }
-    public void bt3 (View view) {
+
+    public void bt3(View view) {
         //Toast.makeText(this,"Click en el boton",Toast.LENGTH_SHORT).show();
         //Log.d(Tag, "Click en el boton");
-        String cap=display.getText().toString();
-        cap=cap+"3";
+        String cap = display.getText().toString();
+        cap = cap + "3";
         display.setText(cap);
     }
-    public void bt2 (View view) {
+
+    public void bt2(View view) {
         //Toast.makeText(this,"Click en el boton",Toast.LENGTH_SHORT).show();
         //Log.d(Tag, "Click en el boton");
-        String cap=display.getText().toString();
-        cap=cap+"2";
+        String cap = display.getText().toString();
+        cap = cap + "2";
         display.setText(cap);
     }
-    public void bt1 (View view) {
+
+    public void bt1(View view) {
         //Toast.makeText(this,"Click en el boton",Toast.LENGTH_SHORT).show();
         //Log.d(Tag, "Click en el boton");
-        String cap=display.getText().toString();
-        cap=cap+"1";
+        String cap = display.getText().toString();
+        cap = cap + "1";
         display.setText(cap);
     }
-    public void bt0 (View view) {
+
+    public void bt0(View view) {
         //Toast.makeText(this,"Click en el boton",Toast.LENGTH_SHORT).show();
         //Log.d(Tag, "Click en el boton");
-        String cap=display.getText().toString();
-        cap=cap+"0";
+        String cap = display.getText().toString();
+        cap = cap + "0";
         display.setText(cap);
     }
-    public void btp (View view) {
+
+    public void btp(View view) {
         //Toast.makeText(this,"Click en el boton",Toast.LENGTH_SHORT).show();
         //Log.d(Tag, "Click en el boton");
-        String cap=display.getText().toString();
-        cap=cap+".";
+        String cap = display.getText().toString();
+        cap = cap + ".";
         display.setText(cap);
     }
+
     public void saved() {
         if (op == 1) {
             result_c = op1 + result_c;
@@ -209,14 +234,19 @@ public class MainActivity extends AppCompatActivity {
             result_c = op1 - result_c;
         }
     }
+
     public void saved2() {
-        result_s[0] = op1;
-        result_s[1] = op2;
-        result_s[i] = result_c;
-        i++;
-        if (i == 9) {
-        i =0;
-        }
+        result_s.add(op1);
+        result_s.add(op2);
+        result_s.add(result_c);
     }
+
+    public void activity2(View view) {
+        Intent k = new Intent(this, Main2Activity.class);
+        //String aux3 = display.getText().toString();
+        //k.putExtra("val1", aux3);
+        startActivity(k);
+    }
+
 }
 
